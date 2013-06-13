01c8cd728d26ae14d1bbedf6a8c87f9d5d82a798
Paul Brian (lifeisstillgood) <paul@mikadosoftware.com>
2013-06-13 14:35:25 +0100

    make referesh and server run live ini files

--- mikadocms/data/oss4gov.org/refreshAndServe.sh
+++ mikadocms/data/oss4gov.org/refreshAndServe.sh
@@ -1,4 +1,4 @@
 git pull origin master
-python orig2htm.py data/oss4gov.org/oss.ini
-python cmsapp.py --config=data/oss4gov.org/oss.ini
+python orig2htm.py data/oss4gov.org/oss_live.ini
+python cmsapp.py --config=data/oss4gov.org/oss_live.ini
 
