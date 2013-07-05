14e9b4072613e97107ff8b5055b258d986c753fe
Paul Brian (lifeisstillgood) <paul@mikadosoftware.com>
2013-07-05 16:30:55 +0100

    split into two run files

---
+++ mikadocms/runmikado.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source ~/.bash_profile
+
+workon vcms 
+python orig2htm.py data/oss4gov.org/oss.ini
+python orig2htm.py data/mikadosoftware.com/mikado.ini
+
+python cmsapp.py --config=data/mikadosoftware.com/mikado.ini
+#python cmsapp.py --config=data/oss4gov.org/oss.ini
+
